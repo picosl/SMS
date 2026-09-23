4a4a1bf1e8b5981cb9d8445f4f89e5b76adafc7c
Shahriar Mostafiz <shahriarmostafiz19@gmail.com>
2026-09-23 10:54:39 +0600

    Update certificate.component.ts

--- src/app/admin/certificate/certificate/certificate.component.ts
+++ src/app/admin/certificate/certificate/certificate.component.ts
@@ -410,11 +410,14 @@ export class CertificateComponent implements OnInit, OnDestroy {
       case 'C':
         if (this.HFormGroup1.value.certificateReportType === 'certificate_complete') {
         return 'certificate_complete';
-    }
+        }
 
-    if (isCollege23 && isNSW) {
-        return `certificate_nsw_${this.HFormGroup1.value.staffId}`;
-    }
+        if (isCollege23 && isNSW) {
+            return `certificate_nsw_${this.HFormGroup1.value.staffId}`;
+        }
+        if (this.trainingActId.length > 25) {
+        return 'extcertificate';
+        }
         else {
           return `certificate`;
         }
